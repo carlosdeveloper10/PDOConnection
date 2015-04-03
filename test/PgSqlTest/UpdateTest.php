0b00884216220d896262afe102069ecfe63a911b
Carlos Mario <carlos_programmer10@hotmail.com>
2015-04-03 13:53:09 -0500

    pgsql test
    Test with CRUD using postgresql, the tests were successful

---
+++ test/PgSqlTest/UpdateTest.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Copyright (c) Carlos Mario. 
+ * Please, read the document linceseinfo.doc, to find information about license 
+ * of this code 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+//Necessary classes
+use cmsoft\com\connection\Connection;
+
+//Necessary files in other directories
+require '../lib/connection/Connection.php';
+
+$sql = "UPDATE tbl SET value1 = 'PDOTest'";
+
+/**
+ * The order of the calls are so important.
+ */
+$con = new Connection();
+$con->setJsonPath("connectionparameters.json");
+$con->open();//Always, you must open the DB.
+$con->setSqlStatement($sql);
+$int = $con->executeUpdate();
+$con->close();//It is optional
+
+if ($int > 0) {
+    echo 'The update was successful';
+}else{
+    echo 'Occurred a problem';
+}
+
---
+++ test/PgSqlTest/test.sql
@@ -0,0 +1,15 @@
+--Copyright (c) Carlos Mario. 
+--Please, read the document linceseinfo.doc, to find information about license 
+--of this code 
+
+
+--Architecture info.
+--DBMS: Posgresql
+--VERSION: PostgreSQL 9.4.1, compiled by Visual C++ build 1800, 64-bit
+--OS: Windows 7 professiona x64.
+CREATE DATABASE test
+
+CREATE TABLE tbl(
+    value1 VARCHAR(50),
+    value2 VARCHAR(50)
+);
